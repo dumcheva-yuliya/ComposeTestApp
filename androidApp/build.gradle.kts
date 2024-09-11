b75b9e602d9bc5f13d4ea8428eb5291bba39ea21
Юлия Думчева <uppavlenko@yandex.ru>
2024-09-11 14:27:12 +0300

    Миграция на KMP.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -44,7 +44,8 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+//        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
     packaging {
         resources {
